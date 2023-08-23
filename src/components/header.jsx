de42086838b0f39254166fd498396d76dc445711
Marcin Adamiuk <adamiuk.marcin@gmail.com>
2023-08-23 14:58:46 +0200

    Added cart in navigation with style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
 import logoSVG from '../assets/brand.svg';
+import cartSVG from '../assets/cart-bag.svg';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
-        // Sprawdzanie czy szerokość ekranu jest mniejsza niż 992px (przyjęta granica dla wersji mobilnej)
         const handleWindowSizeChange = () => {
             setIsMobile(window.innerWidth < 992);
         };
 
-        // Wywołanie funkcji po załadowaniu komponentu i przy zmianie szerokości ekranu
         handleWindowSizeChange();
         window.addEventListener('resize', handleWindowSizeChange);
 
-        // Oczyszczenie nasłuchiwania po odmontowaniu komponentu
         return () => {
             window.removeEventListener('resize', handleWindowSizeChange);
         };
@@ -29,29 +27,35 @@ const Header = () => {
     return (
         <header className="header">
             <div className={"container-header"}>
-            <Link to="/" className="logo">
-                {/*<img src={logoSVG} alt="Catering" />*/}
-                <p className={"company-brand"}>Company brand</p>
-            </Link>
-            <nav className={`navigation ${isMobile && menuOpen ? 'open' : ''}`}>
-                <div className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-                    <div className="bar"></div>
-                    <div className="bar"></div>
-                    <div className="bar"></div>
-                </div>
-                <ul className={`nav-list ${menuOpen ? 'open' : ''}`}>
-                    <li className="nav-item">
-                        <Link to="/" onClick={toggleMenu}>Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/about" onClick={toggleMenu}>About</Link>
-                    </li>
-                    <Link to="/logowanie" onClick={toggleMenu}>
-                        <button className={"btn-client"}>panel klienta</button>
-                    </Link>
-                    <button className={"btn-cta"}>zamów online</button>
-                </ul>
-            </nav>
+                <Link to="/" className="logo">
+                    {/*<img src={logoSVG} alt="Catering" />*/}
+                    <p className={"company-brand"}>Company brand</p>
+                </Link>
+                <nav className={`navigation ${isMobile && menuOpen ? 'open' : ''}`}>
+                    <div className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </div>
+                    <ul className={`nav-list ${menuOpen ? 'open' : ''}`}>
+                        <li className="nav-item">
+                            <Link to="/" onClick={toggleMenu}>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/about" onClick={toggleMenu}>About</Link>
+                        </li>
+                        <Link to="/logowanie" onClick={toggleMenu}>
+                            <button className={"btn-client"}>panel klienta</button>
+                        </Link>
+                        <button className={"btn-cta"}>zamów online</button>
+                        {/* Przycisk koszyka jako div */}
+                        <div className="cart-icon">
+                            <img src={cartSVG} alt="Cart"/>
+                            <p className={"cart-title"}>koszyk</p>
+                            <p className={"cart-counter"}>0</p>
+                        </div>
+                    </ul>
+                </nav>
             </div>
         </header>
     );
