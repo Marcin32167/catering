622746c26e861165373048157ff67ee4c8e63727
Marcin Adamiuk <adamiuk.marcin@gmail.com>
2023-09-26 18:20:43 +0200

    changing the icon based on the time of day

--- src/app.jsx
+++ src/app.jsx
@@ -41,7 +41,7 @@ function App() {
     useEffect(() => {
         const favicon = document.getElementById('favicon');
         if (favicon) {
-            favicon.href = isDay ? '/icon-day.svg' : '/icon-night.svg';
+            favicon.href = isDay ? '../src/assets/logo-day.svg' : '../src/assets/logo-night.svg';
         }
     }, [isDay]);
 
